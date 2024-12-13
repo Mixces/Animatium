2b88ec3a0c2aba50d1b5f690ba90a5735bf729a2
lowercasebtw <jejojora2@gmail.com>
2024-12-13 14:17:24 -0500

    Fix chat position issue with autocomplete

--- src/main/java/me/mixces/animatium/mixins/MixinRenderLayer.java
+++ src/main/java/me/mixces/animatium/mixins/MixinRenderLayer.java
@@ -1,6 +1,5 @@
 package me.mixces.animatium.mixins;
 
-import me.mixces.animatium.config.AnimatiumConfig;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderPhase;
 import net.minecraft.client.render.VertexFormat;
@@ -17,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class MixinRenderLayer {
     // TODO/NOTE: uh i couldn't find a better way to do this as everything I tried didn't work :P
     @Unique
-    private static final RenderLayer animatium$customEntityGlint = RenderLayer.of(
+    private static final RenderLayer animatium$customArmorEntityGlint = RenderLayer.of(
             "armor_entity_glint",
             VertexFormats.POSITION_TEXTURE,
             VertexFormat.DrawMode.QUADS,
@@ -34,7 +33,31 @@ public abstract class MixinRenderLayer {
                     .build(false)
     );
 
+    @Unique
+    private static final RenderLayer animatium$customEntityGlint = RenderLayer.of(
+            "entity_glint",
+            VertexFormats.POSITION_TEXTURE,
+            VertexFormat.DrawMode.QUADS,
+            1536,
+            RenderLayer.MultiPhaseParameters.builder()
+                    .program(RenderLayer.ENTITY_GLINT_PROGRAM)
+                    .texture(new RenderPhase.Texture(ItemRenderer.ITEM_ENCHANTMENT_GLINT, TriState.DEFAULT, false))
+                    .writeMaskState(RenderLayer.COLOR_MASK)
+                    .cull(RenderLayer.DISABLE_CULLING)
+                    .depthTest(RenderLayer.EQUAL_DEPTH_TEST)
+                    .transparency(RenderLayer.GLINT_TRANSPARENCY)
+                    .texturing(RenderLayer.ENTITY_GLINT_TEXTURING)
+                    .build(false)
+    );
+
     @Inject(method = "getArmorEntityGlint", at = @At("RETURN"), cancellable = true)
+    private static void animatium$forceItemGlintOnEntityArmor(CallbackInfoReturnable<RenderLayer> cir) {
+//        if (AnimatiumConfig.forceItemGlintOnEntity) {
+//            cir.setReturnValue(animatium$customArmorEntityGlint);
+//        }
+    }
+
+    @Inject(method = "getEntityGlint", at = @At("RETURN"), cancellable = true)
     private static void animatium$forceItemGlintOnEntity(CallbackInfoReturnable<RenderLayer> cir) {
 //        if (AnimatiumConfig.forceItemGlintOnEntity) {
 //            cir.setReturnValue(animatium$customEntityGlint);
