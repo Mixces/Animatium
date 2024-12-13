eea1fde648c58c2521173b864f198a23cccba14c
lowercasebtw <jejojora2@gmail.com>
2024-12-13 14:31:51 -0500

    Fix nametag text whilst sneaking & flying

--- src/main/java/me/mixces/animatium/mixins/MixinEntityRenderer.java
+++ src/main/java/me/mixces/animatium/mixins/MixinEntityRenderer.java
@@ -5,12 +5,24 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import me.mixces.animatium.config.AnimatiumConfig;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.state.EntityRenderState;
+import net.minecraft.client.render.entity.state.LivingEntityRenderState;
+import net.minecraft.entity.EntityPose;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(EntityRenderer.class)
 public abstract class MixinEntityRenderer {
+    @WrapOperation(method = "renderLabelIfPresent", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/entity/state/EntityRenderState;sneaking:Z"))
+    private boolean animatium$sneakAnimationWhileFlying(EntityRenderState instance, Operation<Boolean> original) {
+        if (AnimatiumConfig.sneakAnimationWhileFlying && instance instanceof LivingEntityRenderState livingEntityRenderState) {
+            return livingEntityRenderState.sneaking || livingEntityRenderState.isInPose(EntityPose.CROUCHING);
+        } else {
+            return original.call(instance);
+        }
+    }
+
     @WrapOperation(method = "renderLabelIfPresent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/GameOptions;getTextBackgroundOpacity(F)F"))
     private float animatium$hideNameTagBackground(GameOptions instance, float fallback, Operation<Float> original) {
         if (AnimatiumConfig.hideNameTagBackground) {
