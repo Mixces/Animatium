f6794e095392e710c7260b71488e9095745e4f60
lowercasebtw <jejojora2@gmail.com>
2024-12-17 15:44:22 -0500

    Don't show crossbow usage in GUI

--- src/main/java/me/mixces/animatium/mixins/MixinUsingItemProperty.java
+++ src/main/java/me/mixces/animatium/mixins/MixinUsingItemProperty.java
@@ -5,6 +5,7 @@ import com.llamalad7.mixinextras.sugar.Local;
 import me.mixces.animatium.config.AnimatiumConfig;
 import net.minecraft.client.render.item.property.bool.UsingItemProperty;
 import net.minecraft.item.BowItem;
+import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ModelTransformationMode;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,7 +15,9 @@ import org.spongepowered.asm.mixin.injection.At;
 public abstract class MixinUsingItemProperty {
     @ModifyReturnValue(method = "getValue", at = @At(value = "RETURN"))
     private boolean animatium$getValue(boolean original, @Local(argsOnly = true) ItemStack stack, @Local(argsOnly = true) ModelTransformationMode modelTransformationMode) {
-        if (AnimatiumConfig.disableItemUsingTextureInGui && stack.getItem() instanceof BowItem && modelTransformationMode == ModelTransformationMode.GUI) {
+        if (AnimatiumConfig.disableItemUsingTextureInGui &&
+                (stack.getItem() instanceof BowItem || stack.getItem() instanceof CrossbowItem) &&
+                modelTransformationMode == ModelTransformationMode.GUI) {
             return false;
         } else {
             return original;
