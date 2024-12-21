0292918f5c3beb27268b6b2d0134f658fad16b91
lowercasebtw <jejojora2@gmail.com>
2024-12-20 20:20:14 -0500

    Port ModMenuIntegration to Kotlin

---
+++ src/main/kotlin/me/mixces/animatium/config/ModMenuIntegration.kt
@@ -0,0 +1,12 @@
+package me.mixces.animatium.config
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory
+import com.terraformersmc.modmenu.api.ModMenuApi
+import net.minecraft.client.gui.screen.Screen
+
+class ModMenuIntegration : ModMenuApi {
+    @JvmSynthetic
+    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
+        return ConfigScreenFactory { parent: Screen? -> AnimatiumConfig.getConfigScreen(parent) }
+    }
+}
