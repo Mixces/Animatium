021b203bd5cfa8cce2f2cfef48113ff8889a2fb7
Tellinq <59412384+Tellinq@users.noreply.github.com>
2025-02-05 22:58:24 -0500

    Change formatting

--- src/main/java/btw/mixces/animatium/mixins/renderer/MixinGameRenderer.java
+++ src/main/java/btw/mixces/animatium/mixins/renderer/MixinGameRenderer.java
@@ -74,11 +74,12 @@ public abstract class MixinGameRenderer {
         }
     }
 
-    @WrapOperation(method={"bobHurt"}, at={@At(value="FIELD", target="Lnet/minecraft/world/entity/LivingEntity;hurtTime:I")})
+    @WrapOperation(method = "bobHurt", at= @At(value = "FIELD", target = "Lnet/minecraft/world/entity/LivingEntity;hurtTime:I"))
     private int animatium$hurtTime(LivingEntity instance, Operation<Integer> original) {
         if (AnimatiumClient.getEnabled() && AnimatiumConfig.instance().getOffsetHurtTime()) {
             return Math.max(original.call(instance) - 1, 0);
         }
+
         return original.call(instance);
     }
 
