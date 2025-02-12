0da4858d733dbbf5b689777a166c1e847b4add4c
lowercasebtw <jejojora2@gmail.com>
2025-02-12 00:34:00 -0500

    Fix mistake

--- src/main/java/btw/mixces/animatium/mixins/renderer/MixinTextureStateShard.java
+++ src/main/java/btw/mixces/animatium/mixins/renderer/MixinTextureStateShard.java
@@ -37,12 +37,12 @@ import java.util.Optional;
 @Mixin(RenderStateShard.TextureStateShard.class)
 public class MixinTextureStateShard {
     // TODO/NOTE: Do we need this when we have MixinTextureManager
-//    @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/util/Optional;of(Ljava/lang/Object;)Ljava/util/Optional;"))
-//    private Optional<Object> animatium$useItemGlint(Object value, Operation<Optional<Object>> original) {
-//        if (AnimatiumClient.getEnabled() && AnimatiumConfig.instance().getForceItemGlintOnEntity() && value == ItemRenderer.ENCHANTED_GLINT_ENTITY) {
-//            return Optional.of(ItemRenderer.ENCHANTED_GLINT_ITEM);
-//        } else {
-//            return original.call(value);
-//        }
-//    }
+    @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/util/Optional;of(Ljava/lang/Object;)Ljava/util/Optional;"))
+    private Optional<Object> animatium$useItemGlint(Object value, Operation<Optional<Object>> original) {
+        if (AnimatiumClient.getEnabled() && AnimatiumConfig.instance().getForceItemGlintOnEntity() && value == ItemRenderer.ENCHANTED_GLINT_ENTITY) {
+            return Optional.of(ItemRenderer.ENCHANTED_GLINT_ITEM);
+        } else {
+            return original.call(value);
+        }
+    }
 }
