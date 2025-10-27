dffae8e6b063c514be594a2a5d88cb67ea129256
lowercasebtw <jejojora2@gmail.com>
2025-10-27 03:18:44 -0400

    Restore particle blending

--- src/main/java/btw/mixces/animatium/mixins/v1/entity/particles/MixinLivingEntity.java
+++ src/main/java/btw/mixces/animatium/mixins/v1/entity/particles/MixinLivingEntity.java
@@ -28,24 +28,107 @@ package btw.mixces.animatium.mixins.v1.entity.particles;
 import btw.mixces.animatium.AnimatiumClient;
 import btw.mixces.animatium.config.AnimatiumConfig;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.Holder;
+import net.minecraft.core.particles.ColorParticleOption;
 import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.util.ARGB;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity extends Entity {
+    @Shadow
+    @Final
+    private Map<Holder<MobEffect>, MobEffectInstance> activeEffects;
+
+    @Shadow
+    @Final
+    private static EntityDataAccessor<Boolean> DATA_EFFECT_AMBIENCE_ID;
+
+    @Unique
+    private static final EntityDataAccessor<Integer> animatium$DATA_EFFECT_COLOR_ID =
+            SynchedEntityData.defineId(LivingEntity.class, EntityDataSerializers.INT);
+
     public MixinLivingEntity(EntityType<?> entityType, Level level) {
         super(entityType, level);
     }
 
     @WrapWithCondition(method = "tickEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V"))
-    private boolean animatium$hideFirstpersonParticles(Level instance, ParticleOptions particle, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+    private boolean animatium$hideFirstPersonParticles(Level instance, ParticleOptions particle, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
         final Minecraft client = Minecraft.getInstance();
         return !AnimatiumClient.ENABLED || !AnimatiumConfig.instance().extras.disableFirstPersonParticles || this.getId() != client.player.getId() || !client.options.getCameraType().isFirstPerson();
     }
+
+    @Inject(method = "defineSynchedData", at = @At("TAIL"))
+    private void animatium$defineEffectColorID(SynchedEntityData.Builder builder, CallbackInfo ci) {
+        builder.define(animatium$DATA_EFFECT_COLOR_ID, 0);
+    }
+
+    @Inject(method = "updateSynchronizedMobEffectParticles", at = @At("TAIL"))
+    private void animatium$setEffectData(CallbackInfo ci, @Local List<ParticleOptions> particleOptions) {
+        if (AnimatiumClient.ENABLED && AnimatiumConfig.instance().other.restoreParticleBlending) {
+            this.entityData.set(animatium$DATA_EFFECT_COLOR_ID, animatium$getPotionColor(this.activeEffects.values()));
+        }
+    }
+
+    @WrapOperation(method = "tickEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V"))
+    private void animatium$blendParticles(Level instance, ParticleOptions particle, double x, double y, double z, double r, double g, double b, Operation<Void> original) {
+        int color = this.entityData.get(animatium$DATA_EFFECT_COLOR_ID);
+        if (AnimatiumClient.ENABLED && AnimatiumConfig.instance().other.restoreParticleBlending) {
+            boolean hasAmbience = this.entityData.get(DATA_EFFECT_AMBIENCE_ID);
+            boolean showParticle = this.isInvisible() ? this.random.nextInt(15) == 0 : this.random.nextBoolean();
+            if (hasAmbience) {
+                showParticle &= this.random.nextInt(5) == 0;
+            }
+
+            if (!showParticle || color == 0) {
+                return;
+            }
+
+            particle = ColorParticleOption.create(ParticleTypes.ENTITY_EFFECT, ARGB.color(hasAmbience ? 0.15F : 1.0F, color));
+            r = ARGB.redFloat(color);
+            g = ARGB.greenFloat(color);
+            b = ARGB.blueFloat(color);
+        }
+
+        original.call(instance, particle, x, y, z, r, g, b);
+    }
+
+    @Inject(method = "removeEffectParticles", at = @At("TAIL"))
+    private void animatium$clearEffectData(CallbackInfo ci) {
+        if (AnimatiumClient.ENABLED && AnimatiumConfig.instance().other.restoreParticleBlending) {
+            this.entityData.set(DATA_EFFECT_AMBIENCE_ID, false); // TODO/NOTE: Find out why these removed this/if it's needed
+            this.entityData.set(animatium$DATA_EFFECT_COLOR_ID, 0);
+        }
+    }
+
+    @Unique
+    private static int animatium$getPotionColor(Collection<MobEffectInstance> effects) {
+        return effects.isEmpty() ? 3694022 : PotionContents.getColorOptional(effects).orElse(0);
+    }
 }
+
