c0169162bf0300b9d1183f7a9e0492354684e74d
lowercasebtw <ignzoudy@gmail.com>
2025-12-01 01:26:03 -0500

    Fix fastSmoothLighting

---
+++ src/main/java/org/visuals/legacy/animatium/mixins/v1/rendering/lighting/MixinIndigo.java
@@ -0,0 +1,44 @@
+/**
+ * Animatium
+ * The all-you-could-want legacy animations mod for modern minecraft versions.
+ * Brings back animations from the 1.7/1.8 era and more.
+ * <p>
+ * Copyright (C) 2024-2025 lowercasebtw
+ * Copyright (C) 2024-2025 mixces
+ * Copyright (C) 2024-2025 Contributors to the project retain their copyright
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * "MINECRAFT" LINKING EXCEPTION TO THE GPL
+ */
+
+package org.visuals.legacy.animatium.mixins.v1.rendering.lighting;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.fabricmc.fabric.impl.client.indigo.Indigo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Properties;
+import java.util.function.Function;
+
+@Mixin(Indigo.class)
+public abstract class MixinIndigo {
+    // TODO/NOTE: Gross, but I don't have a choice (breaks fastSmoothLighting), sorry Fabric!
+    @WrapOperation(method = "<clinit>", at = @At(value = "INVOKE", target = "Ljava/util/Properties;computeIfAbsent(Ljava/lang/Object;Ljava/util/function/Function;)Ljava/lang/Object;", ordinal = 0))
+    private static <T> T animatium$forceVanillaAO(Properties instance, Object key, Function<? super Object, ?> mappingFunction, Operation<Object> original) {
+        return (T) "vanilla";
+    }
+}
