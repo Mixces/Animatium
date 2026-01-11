2cdfab37e41c9f83fe4d2f8224b0e5e9e26465aa
lowercasebtw <ignzoudy@gmail.com>
2026-01-10 20:18:13 -0500

    Add MixinWeightedVariantsUnbaked_DisableRandomBlockRotations

---
+++ src/main/java/org/visuals/legacy/animatium/mixins/v1/rendering/blocks/MixinWeightedVariantsUnbaked_DisableRandomBlockRotations.java
@@ -0,0 +1,27 @@
+package org.visuals.legacy.animatium.mixins.v1.rendering.blocks;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.renderer.block.model.BlockStateModel;
+import net.minecraft.client.resources.model.WeightedVariants;
+import net.minecraft.util.random.Weighted;
+import net.minecraft.util.random.WeightedList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.visuals.legacy.animatium.Animatium;
+import org.visuals.legacy.animatium.config.AnimatiumConfig;
+
+import java.util.List;
+
+@Mixin(WeightedVariants.Unbaked.class)
+public abstract class MixinWeightedVariantsUnbaked_DisableRandomBlockRotations {
+	@WrapOperation(method = "bake", at = @At(value = "NEW", target = "(Lnet/minecraft/util/random/WeightedList;)Lnet/minecraft/client/resources/model/WeightedVariants;"))
+	private WeightedVariants animatium$disableRandomBlockRotations(final WeightedList<?> list, final Operation<WeightedVariants> original) {
+		if (Animatium.isEnabled() && AnimatiumConfig.instance().other.disableRandomBlockRotations) {
+			final List<? extends Weighted<?>> items = list.unwrap();
+			return new WeightedVariants(WeightedList.of((List<Weighted<BlockStateModel>>) List.of(items.getFirst())));
+		} else {
+			return original.call(list);
+		}
+	}
+}
