dec11cbd958939a049e8577be2deb9104a65b87f
lowercasebtw <jejojora2@gmail.com>
2026-03-30 21:14:52 -0500

    Re-implement animatium$disableEntityScissor

--- src/main/java/org/visuals/legacy/animatium/mixins/v1/gui/old_inventory_rendering/MixinInventoryScreen_DisableEntityScissor.java
+++ src/main/java/org/visuals/legacy/animatium/mixins/v1/gui/old_inventory_rendering/MixinInventoryScreen_DisableEntityScissor.java
@@ -25,19 +25,29 @@
 
 package org.visuals.legacy.animatium.mixins.v1.gui.old_inventory_rendering;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.navigation.ScreenRectangle;
+import net.minecraft.client.gui.render.state.pip.GuiEntityRenderState;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.visuals.legacy.animatium.Animatium;
+import org.visuals.legacy.animatium.config.AnimatiumConfig;
 
 @Mixin(InventoryScreen.class)
 public abstract class MixinInventoryScreen_DisableEntityScissor {
-    // TODO 3.2
-    /*@WrapWithCondition(method = "renderEntityInInventoryFollowsMouse", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;enableScissor(IIII)V"))
-    private static boolean animatium$entityScissor(GuiGraphics instance, int minX, int minY, int maxX, int maxY) {
-        return !Animatium.isEnabled() || !AnimatiumConfig.instance().other.disableInventoryEntityScissor;
+    @WrapOperation(method = "renderEntityInInventoryFollowsMouse", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;submitEntityRenderState(Lnet/minecraft/client/renderer/entity/state/EntityRenderState;FLorg/joml/Vector3f;Lorg/joml/Quaternionf;Lorg/joml/Quaternionf;IIII)V"))
+    private static void animatium$disableEntityScissor(final GuiGraphics instance, final EntityRenderState renderState, final float scale, final Vector3f translation, final Quaternionf rotation, final Quaternionf overrideCameraAngle, final int x0, final int y0, final int x1, final int y1, final Operation<Void> original) {
+        if (Animatium.isEnabled() && AnimatiumConfig.instance().other.disableInventoryEntityScissor) {
+            final ScreenRectangle bounds = new ScreenRectangle(0, 0, instance.guiWidth(), instance.guiHeight());
+            instance.guiRenderState.submitPicturesInPictureState(new GuiEntityRenderState(renderState, translation, rotation, overrideCameraAngle, x0, y0, x1, y1, scale, bounds));
+        } else {
+            original.call(instance, renderState, scale, translation, rotation, overrideCameraAngle, x0, y0, x1, y1);
+        }
     }
-
-    @WrapWithCondition(method = "renderEntityInInventoryFollowsMouse", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;disableScissor()V"))
-    private static boolean animatium$entityScissor(GuiGraphics instance) {
-        return !Animatium.isEnabled() || !AnimatiumConfig.instance().other.disableInventoryEntityScissor;
-    }*/
 }
