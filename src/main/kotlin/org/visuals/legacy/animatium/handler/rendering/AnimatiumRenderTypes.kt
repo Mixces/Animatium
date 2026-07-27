c066be3c3cd44ddd4007ba40badf268ec2bd2905
lowercasebtw <jejojora2@gmail.com>
2026-07-27 17:56:57 -0400

    Add "Glint Affects Armor Tint" setting
    Signed-off-by: lowercasebtw <jejojora2@gmail.com>

---
+++ src/main/kotlin/org/visuals/legacy/animatium/handler/rendering/AnimatiumRenderTypes.kt
@@ -0,0 +1,48 @@
+/**
+ * Animatium
+ * The all-you-could-want legacy animations mod for modern minecraft versions.
+ * Brings back animations from the 1.7/1.8 era and more.
+ * <p>
+ * Copyright (C) 2024-2025 lowercasebtw
+ * Copyright (C) 2024-2025 mixces
+ * Copyright (C) 2024-2025 Contributors to the project retain their copyright
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * "MINECRAFT" LINKING EXCEPTION TO THE GPL
+ */
+
+package org.visuals.legacy.animatium.handler.rendering
+
+import net.minecraft.client.renderer.RenderPipelines
+import net.minecraft.client.renderer.feature.ItemFeatureRenderer
+import net.minecraft.client.renderer.rendertype.LayeringTransform
+import net.minecraft.client.renderer.rendertype.RenderSetup
+import net.minecraft.client.renderer.rendertype.RenderType
+import net.minecraft.client.renderer.rendertype.TextureTransform
+
+object AnimatiumRenderTypes {
+    // Glint
+    // NOTE: This is just the default armor glint RenderType, but with ``.useOverlay`` added
+    @JvmField
+    val ENTITY_ARMOR_GLINT_OVERLAY = RenderType.create(
+        "animatium_entity_armor_glint_overlay",
+        RenderSetup.builder(RenderPipelines.GLINT)
+            .withTexture("Sampler0", ItemFeatureRenderer.ENCHANTED_GLINT_ARMOR)
+            .setTextureTransform(TextureTransform.ARMOR_ENTITY_GLINT_TEXTURING)
+            .setLayeringTransform(LayeringTransform.VIEW_OFFSET_Z_LAYERING)
+            .useOverlay() // Applies overlay tint
+            .createRenderSetup()
+    );
+}
