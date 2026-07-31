ee2cf1c124f475c457ee3aabc8ea9b1ceb58ff01
lowercasebtw <jejojora2@gmail.com>
2026-07-31 05:15:33 -0400

    Add "Smooth Particles"

--- src/main/java/org/visuals/legacy/animatium/config/category/FixesConfigCategory.java
+++ src/main/java/org/visuals/legacy/animatium/config/category/FixesConfigCategory.java
@@ -45,6 +45,7 @@ public final class FixesConfigCategory extends Category {
     public boolean fixItemUsageVisualInGUI = true;
     public boolean fixDoubleUsageVisual = true;
     public boolean oldSkyRenderingCheck = true;
+    public boolean smoothParticles = true;
 
     public static ConfigCategory create(final FixesConfigCategory defaults, final FixesConfigCategory config) {
         final ConfigCategory.Builder category = ConfigCategory.createBuilder();
@@ -74,6 +75,7 @@ public final class FixesConfigCategory extends Category {
         bundle.booleanEntry("fixItemUsageVisualInGUI");
         bundle.booleanEntry("fixDoubleUsageVisual");
         bundle.booleanEntry("oldSkyRenderingCheck");
+        bundle.booleanEntry("smoothParticles");
 
         return bundle;
     }
